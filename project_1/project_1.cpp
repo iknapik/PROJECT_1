8bf745151073c6ac75effe00ae4a113affdfc918
= <=>
2019-04-14 19:28:45 +0200

    added some comments to explain how it works

--- project_1/project_1.cpp
+++ project_1/project_1.cpp
@@ -30,22 +30,45 @@ public:
 using namespace school;
 using namespace cheshire;
 
-
-int main()
+void clean_db()
 {
 	std::remove("students_db.txt");
 	std::remove("id_students_db.txt");
-	Timer timer{};
-	DatabaseModel db{};
-	StudentInfo stud1{"Michal", "Kowalski", "9123819", "Wadowice", "Wiejska 3", 1};
-	ClassInfo class1{"16i", 3};
-	ProfessorInfo prof1{"Tomasz", "Wozniak", "Mgr", {"MD", "PE"}};
-	MarkInfo mark1{4, MARK::_3, "04-01-2019", SUBJECT::MD, 3};
-	//db.add(mark1);
-	//db.add(stud1);
-	//db.add(class1);
-	//db.add(prof1);
+	std::remove("marks_db.txt");
+	std::remove("id_marks_db.txt");
+	std::remove("professors_db.txt");
+	std::remove("id_professors_db.txt");
+	std::remove("classes_db.txt");
+	std::remove("id_classes_db.txt");
+}
 
+int main()
+{
+	//dla testów usuwa wszystkie pliki bazy danych
+	clean_db();
+
+	//dla testów
+	Timer timer{};
+
+	// sworzenie instancji bazy danych potrzebne do manipulacji bazą danych
+	DatabaseModel db{};
+
+	// tworzymy studenta {Imie, Nazwisko, PESEL, Miejscowośc, Ulica, opcjonalnie ID klasy}
+	StudentInfo stud1{"Michal", "Kowalski", "9123819", "Wadowice", "Wiejska 3", 1};
+	//tworzymy klase{Nazwa, który semestr}
+	ClassInfo class1{"16i", 3};
+	//tworzymy profesore{Imie, Nazwisko, Tytuł, {przedm1, przedm2, ...}}
+	ProfessorInfo prof1{"Tomasz", "Wozniak", "Mgr", {"MD", "PE"}};
+	//tworzymy ocene{id studenta, enum MARK - czyli ocena, data, enum SUBJECT - czyli przedmiot, id nauczyciela}
+	MarkInfo mark1{4, MARK::_3, "04-01-2019", SUBJECT::MD, 3};
+	
+	//dodajemy do bazy danych za pomocą przeładowanych funkcji
+	db.add(mark1);
+	db.add(stud1);
+	db.add(class1);
+	db.add(prof1);
+
+	//dodajemy dużo studentów dla sprawdzenia wydajności bazy danych
 	for (int i = 1; i < 1000; ++i)
 	{
 		db.add(stud1);
@@ -54,7 +77,8 @@ int main()
 	}
 
 	std::string str = "16i";
-	//std::getline(std::cin, str);
+	
+	// wyciągamy studentów z danej klasy
 	for (auto &stud : db.get_students_by_class(str))
 	{
 		std::cout << stud << "\n";
